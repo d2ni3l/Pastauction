f22efea501aa70c56fb0ee217ba89afea77351d3
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-08-25 12:01:24 +0200

    added hide function to model vehicle card

--- src/components/datas/ModelVehicles.tsx
+++ src/components/datas/ModelVehicles.tsx
@@ -12,10 +12,10 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { ProgressRed } from "../ui/progressRed";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-
+import { inter } from "@/app/fonts";
 export default function ModelVehicles() {
   return (
-    <div>
+    <div className={`${inter.className} pb-5`}>
       <div className='flex gap-5 lg:gap-10 flex-col lg:flex-row'>
         <div className='flex  gap-2 items-center'>
           <Image
@@ -25,12 +25,12 @@ export default function ModelVehicles() {
             height='50'
           />
           <Link href='/datas/brandproduction'>
-          <Image
-            src='/images/jaguarlogo.svg'
-            alt='brand logo'
-            width='100'
-            height='50'
-          />
+            <Image
+              src='/images/jaguarlogo.svg'
+              alt='brand logo'
+              width='100'
+              height='50'
+            />
           </Link>
         </div>
 
@@ -50,15 +50,17 @@ export default function ModelVehicles() {
             </div>
           </div>
 
-          <div className='flex flex-col gap-2'>
-            <div className='flex gap-4 items-center'>
+          <div className='flex flex-col gap-2 items-center'>
+            <div className='flex gap-4 items-center '>
               <p className='text-sm text-gray-500 italic'>from</p>
               <button className='border-[1.6px] border-black rounded-sm text-left cursor-default font-medium py-[2px] w-[180px] px-2'>
                 1969
               </button>
             </div>
             <div className='flex gap-4 items-center'>
-              <p className='text-sm text-gray-500 italic'>to</p>
+              <p className='text-sm text-gray-500 italic'>
+                <span className='select-none text-transparent'>pa</span>to
+              </p>
               <button className='border-[1.6px] border-black rounded-sm text-left cursor-default font-medium py-[2px] w-[180px] px-2'>
                 1970
               </button>
@@ -169,46 +171,61 @@ export default function ModelVehicles() {
 }
 
 const CardModel = () => {
+  const [hide, setHide] = React.useState(false);
   return (
-    <div className='bg-white  shadow-md px-3 py-4'>
+    <div className={`bg-white  shadow-md px-3 py-4 `}>
       <div className='flex flex-col lg:flex-row lg:justify-between'>
-
         <div className='flex flex-col lg:flex-row gap-2 xl:gap-3'>
           <div>
             {" "}
             <Image
               src='/images/modelVehicleplaceholder.svg'
               alt='car image'
-              width='300'
-              height='200'
+              width={hide ? "200" : "300"}
+              height={hide ? "150" : "200"}
             />
           </div>
 
           <div>
-            <div className='gap-5 flex xl:items-center pt-2'>
-              <h2 className='font-bold text-lg sm:text-xl'>Rolls Royce Motors Cars</h2>
-              <h2 className='font-medium'>1984</h2>
-            </div>
-            <div className='pt-5 flex gap-2'>
-              <p className='font-semibold text-sm sm:text-base'>CAMARGUE</p>
-              <p className='font-semibold text-sm'></p>
-            </div>
-            <div className='pt-5 flex gap-5'>
+            {!hide && (
+              <div className='gap-5 flex xl:items-center pt-2'>
+                <h2 className='font-bold text-lg sm:text-xl'>
+                  Rolls Royce Motors Cars
+                </h2>
+                <h2 className='font-medium'>1984</h2>
+              </div>
+            )}
+
+            {!hide && (
+              <div className='pt-5 flex gap-2 items-center xl:gap-[120px]'>
+                <p className='font-semibold text-sm sm:text-base'>CAMARGUE</p>
+                <p className='font-medium  text-sm'>Retractable Hardtop</p>
+              </div>
+            )}
+
+            <div className='pt-5 flex gap-5 items-center'>
+              {hide && <h2 className='font-medium'>1984</h2>}
+
               <div className='flex gap-3'>
                 <p className=' text-gray-600 text-sm italic'>chassis n.</p>
                 <p className='text-black text-sm font-medium'>
                   SCAYJ42A0ECX07948
                 </p>
               </div>
-              <div>
-                <p className='text-gray-600 text-sm italic'>Engine n.</p>
-              </div>
+
+              {!hide && (
+                <div>
+                  <p className='text-gray-600 text-sm italic'>Engine n.</p>
+                </div>
+              )}
             </div>
 
             <div className='border border-gray-300 mt-5 mb-5 max-w-[450px]'></div>
 
             <div className='grid grid-cols-2 sm:grid-cols-3 gap-5 items-center'>
-              <p className='text-sm font-semibold sm:text-base'>Gooding & Company</p>
+              <p className='text-sm font-semibold sm:text-base'>
+                Gooding & Company
+              </p>
               <p className='flex text-gray-500 gap-2'>
                 <span className='italic'>Scottdale</span>
                 <span className='text-black'>2010</span>
@@ -230,55 +247,64 @@ const CardModel = () => {
               <p className='font-semibold text-black lg:pt-5 lg:pb-5 py-2'>
                 63.060
               </p>
-              <div className='relative '>
-                <Progress
-                  value={93}
-                  className='w-[100%] lg:w-[140%] h-[30px] rounded-sm '
-                />{" "}
-                <span className='absolute top-[15%] right-0 font-medium'>
-                  93%
-                </span>
-              </div>
+              {!hide && (
+                <div className='relative '>
+                  <Progress
+                    value={93}
+                    className='w-[100%] lg:w-[140%] h-[30px] rounded-sm '
+                  />{" "}
+                  <span className='absolute top-[15%] right-0 font-medium'>
+                    93%
+                  </span>
+                </div>
+              )}
             </div>
             <div className='flex flex-col gap-2'>
               <p className='text-gray-500 sm:text-sm '>EUR max</p>
               <p className='font-semibold text-black lg:pt-5 lg:pb-5 py-2'>
                 77.060
               </p>
-              <div className='relative'>
-                <Progress
-                  value={69}
-                  className='w-[100%] lg:w-[140%] h-[30px] rounded-sm '
-                />{" "}
-                <span className='absolute top-[15%] right-0 font-medium'>
-                  69%
-                </span>
-              </div>
+              {!hide && (
+                <div className='relative'>
+                  <Progress
+                    value={69}
+                    className='w-[100%] lg:w-[140%] h-[30px] rounded-sm '
+                  />{" "}
+                  <span className='absolute top-[15%] right-0 font-medium'>
+                    69%
+                  </span>
+                </div>
+              )}
             </div>
             <div className='flex flex-col gap-2'>
               <p className='text-gray-500 sm:text-sm '>EUR sale</p>
               <p className='font-semibold text-black lg:pt-5 lg:pb-5 py-2'>
                 24.060
               </p>
-              <div className='relative'>
-                <ProgressRed
-                  value={35}
-                  className='w-[100%] lg:w-[140%] h-[30px] rounded-sm '
-                />{" "}
-                <span className='absolute top-[15%] right-0 font-medium'>
-                  35%
-                </span>
-              </div>
+              {!hide && (
+                <div className='relative'>
+                  <ProgressRed
+                    value={35}
+                    className='w-[100%] lg:w-[140%] h-[30px] rounded-sm '
+                  />{" "}
+                  <span className='absolute top-[15%] right-0 font-medium'>
+                    35%
+                  </span>
+                </div>
+              )}
             </div>
           </div>
 
-          <div className='flex gap-4 justify-end pt-3 lg:pt-12'>
+          <div className={`flex gap-4 justify-end pt-3 ${!hide && 'lg:pt-12'}`}>
             <Image
               className='cursor-pointer scale-hover'
               src='/images/dash-circle-fill.svg'
               width='30'
               height='30'
               alt='dash'
+              onClick={() => {
+                setHide(!hide);
+              }}
             />
             <Image
               className='cursor-pointer scale-hover'
@@ -289,7 +315,6 @@ const CardModel = () => {
             />
           </div>
         </div>
-
       </div>
     </div>
   );
